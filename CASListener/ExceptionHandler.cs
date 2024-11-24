6e01e42f346ab9706366d4043165a5cda22af802
Arek <Arek@DESKTOP-LSNV9UL>
2024-11-24 19:33:15 +0100

    Exception handler spam protection.

--- CASListener/ExceptionHandler.cs
+++ CASListener/ExceptionHandler.cs
@@ -2,6 +2,7 @@
 using Sims3.SimIFace;
 using Sims3.UI;
 using System;
+using System.Collections.Generic;
 
 namespace Arro.MCR
 {
@@ -9,6 +10,7 @@ namespace Arro.MCR
     {
         public static string functionErrorName;
         public static Exception exception;
+        private static List<string> reportedErrors = new List<string>();
 
         public static void WriteErrorXMLFile(string fileName, Exception errorToPrint)
         {
@@ -22,13 +24,29 @@ namespace Arro.MCR
                 customXmlWriter.WriteEndDocument();
             }
         }
+
         public static void HandleException(Exception ex, string functionName)
         {
             functionErrorName = functionName;
             exception = ex;
+
+            // Generate a unique signature for the exception
+            string errorSignature = $"{functionName}|{ex.Message}|{ex.StackTrace}";
+
+            // Check if this error has already been reported
+            if (reportedErrors.Contains(errorSignature))
+            {
+                return; // Suppress duplicate notification
+            }
+
+            // Add the error signature to the list of reported errors
+            reportedErrors.Add(errorSignature);
+
+            // Show the notification
             ExceptionHandler buttonNotification = new ExceptionHandler();
-            buttonNotification.ShowButtonNotification(); // This will invoke the notification
+            buttonNotification.ShowButtonNotification();
         }
+
         public void ShowButtonNotification()
         {
             string titleText = "Error occurred while executing " + functionErrorName + ". Click button below to save exception info to The Sims 3 folder.";
@@ -41,10 +59,14 @@ namespace Arro.MCR
             format.mCloseOnCallback = true;
             StyledNotification.Show(format, "arro_error_icon");
         }
+
         public void ButtonCallback()
         {
+            // Save the error details to file
             ExceptionHandler.WriteErrorXMLFile(functionErrorName + "_error", exception);
-            return;
+
+            // Clear the List to allow re-notification for the same error
+            reportedErrors.Clear();
         }
     }
 }
